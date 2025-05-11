7e43c127406e8f5b3289a79e8e8885f82d97ffc2
Daisy Zhang <daisyzhang2028@u.northwestern.edu>
2025-05-10 19:12:05 -0500

    finish the challenge 4

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import GAMES_DATA from './games.js';
 
 // create a list of objects to store the data about the games using JSON.parse
 const GAMES_JSON = JSON.parse(GAMES_DATA)
-
 // remove all child elements from a parent element in the DOM
 function deleteChildElements(parent) {
     while (parent.firstChild) {
@@ -72,19 +71,17 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
-
+const totalContribution = GAMES_JSON.reduce((total, game) => total + game.backers, 0);
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
-
+contributionsCard.innerHTML = totalContribution.toLocaleString();
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
-
+const totalPledged = GAMES_JSON.reduce((total, game) => total + game.pledged, 0);
+raisedCard.innerHTML = `$${totalPledged.toLocaleString()}`;
 // set inner HTML using template literal
-
-
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = GAMES_JSON.length.toLocaleString();
 
 
 /*************************************************************************************
